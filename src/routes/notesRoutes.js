b511ebe3a7491e769716a2f10ad4b6dc20a8ce35
Roman <motkoroma2702@gmail.com>
2025-11-08 18:14:35 +0100

    rework project with controllers and routes

---
+++ src/routes/notesRoutes.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import { getAllNotes, getNoteById } from '../controllers/notesController.js';
+
+const router = Router();
+
+router.get('/notes', getAllNotes);
+
+router.get('/notes/:noteId', getNoteById);
+
+export default router;
