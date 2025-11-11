8e9d8f6967af8a13bb5b775d3f10d737c54cad8f
Roman <motkoroma2702@gmail.com>
2025-11-11 08:59:51 +0100

    corrected db and models

--- src/models/note.js
+++ src/models/note.js
@@ -2,7 +2,7 @@ import { Schema, model } from 'mongoose';
 
 const noteSchema = new Schema(
   {
-    title: { type: String, require: true, trim: true },
+    title: { type: String, required: true, trim: true },
     content: { type: String, default: '', trim: true },
     tag: {
       type: String,
