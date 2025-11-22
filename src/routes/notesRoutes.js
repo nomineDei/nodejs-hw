5990911fdd5dd3c121753b1a58a2a93b8d97d117
Roman <motkoroma2702@gmail.com>
2025-11-22 14:21:34 +0100

    corrected validation in routes

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -12,7 +12,7 @@ import {
   getAllNotesSchema,
   noteIdSchema,
   updateNoteSchema,
-} from '../validations/notesValidations.js';
+} from '../validations/notesValidation.js';
 
 const router = Router();
 
