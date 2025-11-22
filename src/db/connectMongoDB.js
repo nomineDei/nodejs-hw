0873a18267d4073a1f2146aaab379b7617b73a35
Roman <motkoroma2702@gmail.com>
2025-11-22 13:57:12 +0100

    add search

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose';
+import { Note } from '../models/note.js';
 
 export const connectMongoDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
     console.log('✅ MongoDB connection established successfully');
+
+    await Note.syncIndexes();
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
     process.exit(1);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import { errors } from 'celebrate';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
@@ -17,6 +18,7 @@ app.use(logger);
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
+app.use(errors());
 app.use(errorHandler);
 
 await connectMongoDB();
